1d987f8752ab4620ffa1c28b8b278314eac59881
Devstack <natanvdb@hotmail.com>
2024-07-17 12:26:00 +0200

    Update update-profile.post.ts

--- server/api/update-profile.post.ts
+++ server/api/update-profile.post.ts
@@ -1,10 +1,7 @@
 import { getServerSession } from "#auth";
 import type { Session } from "next-auth";
-import { resend } from "~/lib/resend";
 import { prisma } from "~/lib/prisma";
 import { userNameSchema } from "~/lib/validations/user";
-import { render } from "@vue-email/render";
-import Test from "~/emails/test.vue";
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
   const session = (await getServerSession(event)) as Session;
@@ -20,22 +17,6 @@ export default defineEventHandler(async (event) => {
     where: { id: session.user.id },
     data: { name },
   });
-  try {
-    const html = await render(Test, {
-      title: "some title",
-    });
-
-    const data = await resend.emails.send({
-      from: "Acme <onboarding@resend.dev>",
-      to: ["delivered@resend.dev"],
-      subject: "Hello world",
-      html,
-    });
-
-    return data;
-  } catch (error) {
-    return { error };
-  }
 
   return {
     status: 200,
